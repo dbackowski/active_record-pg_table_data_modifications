d8ce0a98c9e311cb0e0663e65dea22aaf13c9b22
Damian Baćkowski <dbackowski@artcom.pl>
2015-06-18 09:36:10 +0200

    fix bugs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_record/pg_table_data_modifications'
 
 ActiveRecord::Base.establish_connection(adapter: 'postgresql',
-                                        database: 'active_record-pg_table_data_modifications_test',
+                                        database: 'active_record_pg_table_data_modifications_test',
                                         username: 'postgres')
 
 load File.dirname(__FILE__) + '/support/schema.rb'
